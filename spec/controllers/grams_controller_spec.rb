fc6e31d253a0b5cfbf2b0924d7cc550c8995036e
nevillealee <nevillealee@gmail.com>
2017-08-25 07:16:10 +0000

    rspec grams#edit testing

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -5,6 +5,27 @@ require 'rails_helper'
 #however these strings should be descriptive for other developers reading code
 
 RSpec.describe GramsController, type: :controller do
+    describe "grams#edit action" do
+        it "should successfully show the edit form if the gram is found" do
+        end
+        
+        it "should return a 404 error message if the gram is not found" do
+        end
+    end
+    
+    describe "grams#show action" do
+        it "should successfully show the page if the gram is found" do
+            gram = FactoryGirl.create(:gram)
+            get :show, params: { id: gram.id}
+            expect(response).to have_http_status(:success)
+        end
+        
+        it "should return a 404 error if the gram is not found" do
+            get :show, params: { id: 'TACOTACO'}
+            expect(response).to have_http_status(:not_found)
+        end
+    end
+    
     describe "grams#index action" do
         it "should successfully show the page" do
             get :index  #this line will trigger an HTTP GET request to the index action of our controller
--- spec/factories.rb
+++ spec/factories.rb
@@ -6,6 +6,12 @@ FactoryGirl.define do
       password "securePassword"
       password_confirmation "securePassword"
     end
+    
+    factory :gram do
+      message "hello"
+      #automatically connect the user_id of the model to a user that FactoryGirl will automatically hook up.
+      association :user
+    end
   end
   
   #store user in db with 'FactoryGirl.create(:user)' command
