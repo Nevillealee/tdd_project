f0afce7454a0f33c409188d1dba1440157e8d111
nevillealee <nevillealee@gmail.com>
2017-08-28 03:10:42 +0000

    error in comments#controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe CommentsController, type: :controller do
     describe "comments#create action" do
         it "should allow users to create comments on grams" do
             gram = FactoryGirl.create(:gram)
-            
+            #comment = FactoryGirl.create(:comment)
             user = FactoryGirl.create(:user)
             sign_in user
             
             post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
+            expect(response).to redirect_to root_path
             expect(gram.comments.length).to eq 1
             expect(gram.comments.first.message).to eq "awesome gram"
         end
